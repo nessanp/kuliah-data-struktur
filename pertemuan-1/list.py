3083bd5ec82eceaae999e5854bf67f627aa53d7b
Nessa <nessanirmala29@gmail.com>
2025-03-05 11:09:47 +0700

    first

---
+++ pertemuan-1/list.py
@@ -0,0 +1,22 @@
+# Membuat Variablel
+# String Variable
+namaMhs = "Rizky"
+print(namaMhs)
+print(type(namaMhs))
+#Variabel Interger
+umurmhs = 20
+print(umurmhs)
+print(type(umurmhs))
+#Variable Float
+ipkmhs =3.5
+print(ipkmhs)
+print(type(ipkmhs))
+#Variable Boolean 
+lulusmhs = 3.5>3.0
+print(lulusmhs)
+print(type(lulusmhs))
+
+# Membuat List
+myList = ["Hello Word", 20, 3.5, True]
+print(myList)    
+print(type(myList))

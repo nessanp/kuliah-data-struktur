28b2f64c278e4d15bca3089003f6f830eead116f
Nessa <nessanirmala29@gmail.com>
2025-04-10 15:33:31 +0700

    pertemuan 3

--- Pertemuan-3/dashboard.py
+++ Pertemuan-3/dashboard.py
@@ -1,21 +1,19 @@
 import streamlit as st 
 from ptmn3 import dataMhs
-# Membuat Judul Web Appps
-st.title ("👨‍🎓Manajemen Data Mahasiswa👩‍🎓")
+# Membuat Judul Web Apps
+st.title ("👨🏻‍🎓Manajemen Data Mahasiswa 👩🏻‍🎓")
 # Menambah Data Mahasiswa dengan Append
-st.title("➕ Tambah data denagan Append")
+st.subheader("➕ Tambah data dengan Append")
 new_name = st.text_input("Masukan Nama Mhs")
 new_nim = st.text_input("Masukan NIM")
 new_prodi = st.text_input("Masukan Prodi")
 if st.button("Tambah Append"):
-             new_Mhs = {"nama":new_name, "NIM":new_nim, "Prodi":new_prodi}
-             dataMhs.append(dataMhs)
-             st.success("✅ Data Mahasiswa Berhasil Ditambah")
-
-
+    new_Mhs = {"nama":new_name, "NIM":new_nim, "Prodi":new_prodi}
+    dataMhs.append(new_Mhs)
+    st.success("✅ Data Mahasiswa berhasil ditambah")
 # Menampilkan data Mahasiswa
-st.subheader("📅 Data Mahasiswa")
+st.subheader("📅Data Mahasiswa")
 no=0
 for i in dataMhs:
-    no=no+1
-    st.write(f"{no}. Nama = {i['nama']} NIM = {i['NIM']} Prodi = {i['Prodi']}")
+    no+=1
+    st.write(f"{no}.Nama = {i['nama']};  NIM = {i['NIM']}; Prodi = {i['Prodi']}")
--- Pertemuan-3/ptmn3.py
+++ Pertemuan-3/ptmn3.py
@@ -1,48 +1,49 @@
-# membuat List 
+# Membuat List 
 myList = ["Mahardika", 1, True, 4.6]
-#List Bersifat mutable / indexing
-#untuk menampilkan isi list index ke 0
+# List Besifat mutable / indexing
+# untuk menampilkan isi list index ke 0
 print(myList[0])
 print(myList[-4])
 
-# 3.sifat Slicing 
-# Manampilkan index ke 1 sampai 3
-print(f"menampilakan index ke 0-2 {myList[0:3]}")
-print(f"dari index awal samapi ke 2 {myList[ :3]}")
-dataMhs =[{"nama":"Budi", "NIM":"12345", "Prodi":"Informatika",},
-          {"nama":"Andi","NIM":"123456", "Prodi":"Informatika"}]
+# 3.Sifat Slicing
+# Menampilkan index ke 1 sampai 3
+print(f"menampilkan index ke 0-2 {myList[0:3]}")
+print(f"dari index awa sampai ke index 2 {myList[:3]}")
+dataMhs =[{"nama":"Budi", "NIM":12345, "Prodi":"Informatika"},
+          {"nama":"Andi","NIM":12346, "Prodi":"Informatika"}]
+# menampilkan list berdasar index
 
-# menampilkan list berdasarkan index
+# menambahkan nomor urut pada list
 no=0
 for i in dataMhs:
-    no=no+1
-    print(f"{no}. Nama = {i['nama']} NIM = {i['NIM']} Prodi = {i['Prodi']}")
-# sifat merubah isi List
+    no+=1
+    print(f"{no}.Nama = {i['nama']} NIM = {i['NIM']} Prodi = {i['Prodi']}")
+# Sifat merubah isi List
 myList[0] = "Informatika"
-print(type(dataMhs))
-# menghitung jumlah data dalam list
-print(f"Jumlah elemen dalam data List dataMhs = {len(dataMhs)}")
-print(f"Jumlah elemen dalam data List myList = {len(myList)}")
-# Menambahkan Elemen dalam list
+# print(type(dataMhs))
+# Menghitung jumlah data dalam list
+# print(f"Jumlah elemen dalam data List dataMhs = {len(dataMhs)}")
+# print(f"Jumlah elemen dalam data List myList = {len(myList)}")
+# Menambahkan Elemen dalam list 
 print(myList)
 myList.insert(0,"Mahardika")
-print(f"List setelah ditambah didepan{myList}")
+print(f"List setelah ditambah didepan{myList}") 
 # Menambahkan elemen di belakang list
 myList.append("ITEKES")
 print(myList)
-# Menghapus list dengan remove 
-myList.remove("ITEKES")
+# Menghapus list dengan remove
+myList.remove(4.6)
 print(myList)
 myList.pop(4)
 print(myList)
-# Mengurutkan List
+# Mengurutkan List 
 listWarna = ["Merah", "Kuning", "Hijau", "Biru"]
 listWarna.sort()
 print(listWarna)
 # Membalik List
 listWarna.reverse()
 print(listWarna)
-# Menjumlahkan isi List
+# Menjumlahkan isi List 
 listAngka = [1,2,3,4,5]
 print(sum(listAngka))
 # Membuat salinan dengan copy
